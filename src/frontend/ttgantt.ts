df13fe146c08da1180c7364b3d10d34be1bb002b
ryon <ryon.rmt@gmail.com>
2020-04-25 20:10:26 +0900

    Add drawing month-year

--- src/frontend/ttgantt.ts
+++ src/frontend/ttgantt.ts
@@ -46,10 +46,31 @@ function drawTimeline(gantt: TTGantt) {
   let d: Date = new Date(gantt.startDate.getTime());
   let chartElement = document.createElement('div');
   chartElement.className = 'day-area';
+  let mycount: { [key: string]: number; } = {};
   for(let i = 0; i <= termDay; i++) {
-    // TODO: count month-year
+    let my = getMonthYear(d);
+    if(mycount[my]) {
+      mycount[my]++;
+    } else {
+      mycount[my] = 1;
+    }
     chartElement.insertAdjacentHTML('beforeend', `<div style="height:${height}px;" class="day">${d.getDate()}</div>`);
     d.setDate(d.getDate() + 1);
   }
   dayObj.appendChild(chartElement);
+  const myObj = document.getElementById("month-year-area");
+  if (myObj === null) {
+    return;
+  }
+  myObj.innerHTML = '';
+  for (let key in mycount) {
+    let width = mycount[key] * 15 - 1;
+    myObj.insertAdjacentHTML('beforeend', `<div style="width:${width}px;" class="month-year">${key}</div>`);
+  }
+}
+
+function getMonthYear(d: Date): string {
+  let y = d.getFullYear();
+  let m = d.getMonth() + 1;
+  return String(m) + '-' + String(y);
 }
